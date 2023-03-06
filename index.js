6abc3ac34b9e526d2ca64256326c15ff3825d6d9
ThanhVan113 <114211690+ThanhVan113@users.noreply.github.com>
2023-03-06 20:39:52 +0700

    upload

---
+++ index.js
@@ -0,0 +1,58 @@
+// var i=1
+// for (i=1;i<5;i++){
+//     console.log(i);
+// }
+
+// document.querySelector('p')
+
+// var btn=document.querySelector('button')
+// btn.onclick=function(){
+//     console.log(btn)
+// }
+    
+// var arr=['a','b','c'];
+// arr.map(arr =>{
+//     console.log(arr)
+// })
+
+// var a="k,n,v"
+//     console.log(a)
+
+// var i=['a','b','c','d','e']
+// i.map(i=>{
+//     console.log(i)
+// })
+    
+// var btn=document.querySelector('button')
+//     btn.onclick=function(){
+//         alert('bạn đã kích vào button này')
+//     }
+
+// var btn=document.querySelector('button')
+//     btn.onclick=function(){
+//        document.write('bạn đã kích vào button này')
+//     }
+
+// var img=document.querySelector('img')
+// var btn=document.querySelector('button')
+//     btn.onclick=function(){
+//         img.style.display="none"
+//     }
+
+// var div=document.querySelector('div')
+//     div.onmouseover=function(){
+//         div.style.backgroundColor="red"
+//     }
+//     console.log(div)
+
+// var btn=document.querySelector('button')
+// btn.onclick=function(){
+//     document.write('chào mừng mạnh mát')
+// }
+
+// var input=document.querySelector('input')
+// input.onclick=function(){
+//     input.style.border="red"
+//     input.style.outline="none"
+//     input.style.width="100%"
+// }
